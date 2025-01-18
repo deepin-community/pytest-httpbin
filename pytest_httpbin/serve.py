7da7c3ea431ea1721bd708cc9f8d9ee567ad39e9
Cherrling <cherrling2021@outlook.com>
2025-01-18 15:46:40 +0800

    upgrade pytest-httpbin (2.1.0-1) unstable

--- pytest_httpbin/serve.py
+++ pytest_httpbin/serve.py
@@ -1,34 +1,31 @@
 import os
-import threading
 import ssl
-from wsgiref.simple_server import WSGIServer, make_server, WSGIRequestHandler
+import threading
+from urllib.parse import urljoin
 from wsgiref.handlers import SimpleHandler
-from six.moves.urllib.parse import urljoin
+from wsgiref.simple_server import WSGIRequestHandler, WSGIServer, make_server
 
-
-CERT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'certs')
+CERT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "certs")
 
 
 class ServerHandler(SimpleHandler):
-
-    server_software = 'Pytest-HTTPBIN/0.1.0'
-    http_version = '1.1'
+    server_software = "Pytest-HTTPBIN/0.1.0"
+    http_version = "1.1"
 
     def cleanup_headers(self):
         SimpleHandler.cleanup_headers(self)
-        self.headers['Connection'] = 'Close'
+        self.headers["Connection"] = "Close"
 
     def close(self):
         try:
             self.request_handler.log_request(
-                self.status.split(' ', 1)[0], self.bytes_sent
+                self.status.split(" ", 1)[0], self.bytes_sent
             )
         finally:
             SimpleHandler.close(self)
 
 
 class Handler(WSGIRequestHandler):
-
     def handle(self):
         """Handle a single HTTP request"""
 
@@ -39,7 +36,7 @@ class Handler(WSGIRequestHandler):
         handler = ServerHandler(
             self.rfile, self.wfile, self.get_stderr(), self.get_environ()
         )
-        handler.request_handler = self      # backpointer for logging
+        handler.request_handler = self  # backpointer for logging
         handler.run(self.server.get_app())
 
     def get_environ(self):
@@ -49,55 +46,54 @@ class Handler(WSGIRequestHandler):
         """
         # Note: Can't use super since this is an oldstyle class in python 2.x
         environ = WSGIRequestHandler.get_environ(self).copy()
-        if self.headers.get('content-type') is None:
-            del environ['CONTENT_TYPE']
+        if self.headers.get("content-type") is None:
+            del environ["CONTENT_TYPE"]
         return environ
 
 
 class SecureWSGIServer(WSGIServer):
-
-    def finish_request(self, request, client_address):
-        """
-        Negotiates SSL and then mimics BaseServer behavior.
-        """
-        request.settimeout(1.0)
+    def get_request(self):
+        socket, address = super().get_request()
         try:
-            ssock = ssl.wrap_socket(
-                request,
-                keyfile=os.path.join(CERT_DIR, 'key.pem'),
-                certfile=os.path.join(CERT_DIR, 'cert.pem'),
-                server_side=True,
-                suppress_ragged_eofs=False,
+            socket.settimeout(1.0)
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            context.load_cert_chain(
+                os.path.join(CERT_DIR, "server.pem"),
+                os.path.join(CERT_DIR, "server.key"),
             )
-            self.RequestHandlerClass(ssock, client_address, self)
+            return (
+                context.wrap_socket(
+                    socket, server_side=True, suppress_ragged_eofs=True
+                ),
+                address,
+            )
         except Exception as e:
             print("pytest-httpbin server hit an exception serving request: %s" % e)
             print("attempting to ignore so the rest of the tests can run")
-        # WSGIRequestHandler seems to close the socket for us.
-        # Thanks, WSGIRequestHandler!!
+            raise
+
+    def setup_environ(self):
+        super().setup_environ()
+        self.base_environ["HTTPS"] = "yes"
 
 
-class Server(object):
+class Server:
     """
     HTTP server running a WSGI application in its own thread.
     """
 
-    port_envvar = 'HTTPBIN_HTTP_PORT'
+    port_envvar = "HTTPBIN_HTTP_PORT"
 
-    def __init__(self, host='127.0.0.1', port=0, application=None, **kwargs):
+    def __init__(self, host="127.0.0.1", port=0, application=None, **kwargs):
         self.app = application
         if self.port_envvar in os.environ:
             port = int(os.environ[self.port_envvar])
         self._server = make_server(
-            host,
-            port,
-            self.app,
-            handler_class=Handler,
-            **kwargs
+            host, port, self.app, handler_class=Handler, **kwargs
         )
         self.host = self._server.server_address[0]
         self.port = self._server.server_address[1]
-        self.protocol = 'http'
+        self.protocol = "http"
 
         self._thread = threading.Thread(
             name=self.__class__,
@@ -105,11 +101,21 @@ class Server(object):
         )
 
     def __del__(self):
-        if hasattr(self, '_server'):
+        if hasattr(self, "_server"):
             self.stop()
 
     def start(self):
         self._thread.start()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.stop()
+        suppress_exc = self._server.__exit__(*args, **kwargs)
+        self._thread.join()
+        return suppress_exc
 
     def __add__(self, other):
         return self.url + other
@@ -119,16 +125,16 @@ class Server(object):
 
     @property
     def url(self):
-        return '{0}://{1}:{2}'.format(self.protocol, self.host, self.port)
+        return f"{self.protocol}://{self.host}:{self.port}"
 
     def join(self, url, allow_fragments=True):
         return urljoin(self.url, url, allow_fragments=allow_fragments)
 
 
 class SecureServer(Server):
-    port_envvar = 'HTTPBIN_HTTPS_PORT'
+    port_envvar = "HTTPBIN_HTTPS_PORT"
 
-    def __init__(self, host='127.0.0.1', port=0, application=None, **kwargs):
-        kwargs['server_class'] = SecureWSGIServer
-        super(SecureServer, self).__init__(host, port, application, **kwargs)
-        self.protocol = 'https'
+    def __init__(self, host="127.0.0.1", port=0, application=None, **kwargs):
+        kwargs["server_class"] = SecureWSGIServer
+        super().__init__(host, port, application, **kwargs)
+        self.protocol = "https"
